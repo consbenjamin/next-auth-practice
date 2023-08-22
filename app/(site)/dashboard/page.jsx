9f541a8cf125b6d2d81eefe3301c5b6b673be3ad
consbenjamin <cons_benjamin9@outlook.com>
2023-08-22 15:34:11 -0300

    next-oauth-practice

---
+++ app/(site)/dashboard/page.jsx
@@ -0,0 +1,18 @@
+'use client'
+
+import { useSession, signOut } from "next-auth/react";
+
+import React from 'react'
+
+const dashboard = () => {
+  const {  data:session } = useSession()
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <p>Hi {session?.user?.email}</p>
+      <button onClick={() => signOut()}>Sign Out</button>
+    </div>
+  )
+}
+
+export default dashboard
